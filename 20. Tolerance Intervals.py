e2af1521bd5a91b01f2e7adfa78b2432f01b02f7
Kang-Minseokk <dreampilot920@naver.com>
2024-01-08 13:57:30 +0900

    20. Tolerance Intervals.py

--- 20. Tolerance Intervals.py
+++ 20. Tolerance Intervals.py
@@ -33,3 +33,28 @@ lower, upper = data_mean-interval, data_mean+interval
 print('%.3f to %.2f covers %d%% of data with a confidence of %d%%' % (lower, upper, prop*100, prob*100))
 
 
+
+# 샘플의 크기에 따라서 허용 범위의 크기 변화 측정해보기
+from numpy.random import seed
+from numpy.random import randn
+from numpy import sqrt
+from scipy.stats import chi2
+from scipy.stats import norm
+from matplotlib import pyplot
+
+seed(1)
+sizes = range(5, 15)
+for n in sizes:
+    data = 5 * randn(n) + 50
+    dof = n - 1
+    prop = 0.95
+    prop_inv = (1.0 - prop) / 2.0
+    gauss_critical = norm.ppf(prop_inv)
+
+    prob = 0.99
+    prob_inv = 1.0 - prob
+    chi_critical = chi2.ppf(prop_inv, dof)
+
+    tol = sqrt((dof * (1 + (1/n)) * gauss_critical**2) / chi_critical)
+    pyplot.errorbar(n, 50, yerr=tol, color='blue', fmt='o')
+pyplot.show()
