d882b417d090dc38fdb72814e4eb112bbe7a54d7
Kang-Minseokk <dreampilot920@naver.com>
2024-01-17 16:03:49 +0900

    regression model learning

---
+++ Regression ML/Regression Predictive Modeling.py
@@ -0,0 +1,145 @@
+import numpy
+from numpy import arange
+from matplotlib import pyplot
+import pandas as pd
+from pandas import set_option
+from pandas import read_csv
+from pandas.plotting import scatter_matrix
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import KFold
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import GridSearchCV
+from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import Lasso
+from sklearn.linear_model import ElasticNet
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.neighbors import KNeighborsRegressor
+from sklearn.svm import SVR
+from sklearn.pipeline import Pipeline
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.ensemble import GradientBoostingRegressor
+from sklearn.ensemble import ExtraTreesRegressor
+from sklearn.ensemble import AdaBoostRegressor
+from sklearn.metrics import mean_squared_error
+
+filename = 'housing.csv'
+names = ['CRIM', 'ZN', 'INDUS', 'CHAS', 'NOX', 'RM', 'AGE', 'DIS', 'RAD', 'TAX', 'PTRATIO', 'B', 'LSTAT', 'MEDV']
+dataset = read_csv(filename, delim_whitespace=True, names=names)
+
+# csv파일을 읽어와서 데이터셋에 할당을 해주었으니, 이제는 데이터를 분석한다.
+# 먼저, 데이터의 열과 행 크기를 알아본다. 또한 각 열의 데이터 타입도 알아본다.
+print(dataset.shape)
+print(dataset.dtypes)
+
+# 앞 20행의 데이터를 추출해본다.
+# print(dataset.head(20))
+
+# 데이터셋의 특징을 알아봅니다.
+set_option('display.precision', 1)
+print(dataset.describe())
+
+# 피어슨 상관계수를 사용하여 두 변수간의 선형관계를 파악합니다.
+set_option('display.precision', 2)
+print(dataset.corr(method='pearson'))
+
+# 히스토그램을 그려서 나타낸다.
+dataset.hist(sharex=False, sharey=False, xlabelsize=1, ylabelsize=1)
+pyplot.show()
+
+# 밀도 함수를 그려서 나타내보자.
+dataset.plot(kind='density', subplots=True, layout=(4, 4), sharex=False, legend=False, fontsize=1)
+pyplot.show()
+
+# 박스 수염 그림을 그려보자
+dataset.plot(kind='box', subplots=True, layout=(4,4), sharex=False, sharey=False, fontsize=8)
+pyplot.show()
+
+# scatter 함수를 그려보자.
+scatter_matrix(dataset)
+pyplot.show()
+
+# 상관계수를 매트릭스 위에 나타냅니다.
+fig = pyplot.figure()
+ax = fig.add_subplot(111)
+cax = ax.matshow(dataset.corr(), vmin=-1, vmax=1, interpolation='none')
+fig.colorbar(cax)
+ticks = numpy.arange(0, 14, 1)
+ax.set_xticks(ticks)
+ax.set_yticks(ticks)
+ax.set_xticklabels(names)
+ax.set_yticklabels(names)
+pyplot.show()
+
+
+# Validation dataset을 만들어본다.
+array = dataset.values
+X = array[:, 0:13]
+Y = array[:, 13]
+validation_size = 0.20
+seed = 7
+X_train, X_validation, Y_train, Y_validation = train_test_split(X, Y, test_size=validation_size, random_state=seed)
+#
+num_folds = 10
+seed = 7
+scoring = 'neg_mean_squared_error'
+
+models = [('LR', LinearRegression()), ('LASSO', Lasso()), ('EN', ElasticNet()), ('KNN', KNeighborsRegressor()),
+          ('CART', DecisionTreeRegressor()), ('SVR', SVR())]
+
+results = []
+names = []
+for name, model in models:
+    kfold = KFold(n_splits=num_folds, random_state=seed, shuffle=True)
+    cv_results = cross_val_score(model, X_train, Y_train, cv=kfold, scoring=scoring)
+    results.append(cv_results)
+    names.append(name)
+    msg = "%s: %f (%f)" % (name, cv_results.mean(), cv_results.std())
+    print(msg)
+
+fig = pyplot.figure()
+fig.suptitle('Algorithm Comparison')
+ax = fig.add_subplot(111)
+pyplot.boxplot(results)
+ax.set_xticklabels(names)
+pyplot.show()
+
+
+# 데이터를 표준화할 때 데이터의 누수를 막기 위해서 파이프라인을 사용하도록 한다.
+pipelines = []
+pipelines.append(('ScaledLR', Pipeline([('Scaler', StandardScaler()), ('LR', LinearRegression())])))
+pipelines.append(('ScaledLASSO', Pipeline([('Scaler', StandardScaler()), ('LASSO', Lasso())])))
+pipelines.append(('ScaledEN', Pipeline([('Scaler', StandardScaler()), ('EN', ElasticNet())])))
+pipelines.append(('ScalesKNN', Pipeline([('Scaler', StandardScaler()), ('KNN', KNeighborsRegressor())])))
+pipelines.append(('ScalesCART', Pipeline([('Scaler', StandardScaler()), ('CART', DecisionTreeRegressor())])))
+pipelines.append(('ScalesSVR', Pipeline([('Scaler', StandardScaler()), ('SVR', SVR())])))
+results = []
+names = []
+for name, model in pipelines:
+    kfold = KFold(n_splits=num_folds, random_state=seed, shuffle=True)
+    cv_results = cross_val_score(model, X_train, Y_train, cv=kfold, scoring=scoring)
+    results.append(cv_results)
+    names.append(name)
+    msg = "%s: %f (%f)" % (name, cv_results.mean(), cv_results.std())
+    print(msg)
+
+fig = pyplot.figure()
+fig.suptitle('Scaled Algorithm Comparison')
+ax = fig.add_subplot(111)
+pyplot.boxplot(results)
+ax.set_xticklabels(names)
+pyplot.show()
+
+
+# KNN Algorithm Tuning
+scaler = StandardScaler().fit(X_train)
+rescaledX = scaler.transform(X_train)
+k_values = numpy.array([1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21])
+param_grid = dict(n_neighbors=k_values)
+model = KNeighborsRegressor()
+kfold = KFold(n_splits=num_folds, random_state=seed, shuffle=True)
+grid = GridSearchCV(estimator=model, param_grid=param_grid, scoring=scoring, cv=kfold)
+grid_result = grid.fit(rescaledX, Y_train)
+print("Best: %f using %s" % (grid_result.best_score_, grid_result.best_params_))
+
+
