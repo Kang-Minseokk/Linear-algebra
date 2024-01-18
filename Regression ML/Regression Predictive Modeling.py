c87963ea5c13e339c7132cba7161c490528f3de4
Kang-Minseokk <dreampilot920@naver.com>
2024-01-18 09:38:55 +0900

    Predict House pricing

--- Regression ML/Regression Predictive Modeling.py
+++ Regression ML/Regression Predictive Modeling.py
@@ -1,7 +1,6 @@
 import numpy
 from numpy import arange
 from matplotlib import pyplot
-import pandas as pd
 from pandas import set_option
 from pandas import read_csv
 from pandas.plotting import scatter_matrix
@@ -79,7 +78,7 @@ Y = array[:, 13]
 validation_size = 0.20
 seed = 7
 X_train, X_validation, Y_train, Y_validation = train_test_split(X, Y, test_size=validation_size, random_state=seed)
-#
+
 num_folds = 10
 seed = 7
 scoring = 'neg_mean_squared_error'
@@ -132,6 +131,7 @@ pyplot.show()
 
 
 # KNN Algorithm Tuning
+# KNN에서의 K기본값인 7이 아니라 여러가지 값들을 넣어보고 가장 성능이 좋은 k값을 찾아보도록 한다.
 scaler = StandardScaler().fit(X_train)
 rescaledX = scaler.transform(X_train)
 k_values = numpy.array([1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21])
@@ -140,6 +140,60 @@ model = KNeighborsRegressor()
 kfold = KFold(n_splits=num_folds, random_state=seed, shuffle=True)
 grid = GridSearchCV(estimator=model, param_grid=param_grid, scoring=scoring, cv=kfold)
 grid_result = grid.fit(rescaledX, Y_train)
+
 print("Best: %f using %s" % (grid_result.best_score_, grid_result.best_params_))
+means = grid_result.cv_results_['mean_test_score']
+stds = grid_result.cv_results_['std_test_score']
+params = grid_result.cv_results_['params']
+for mean, stdev, param in zip(means, stds, params):
+    print("%f (%f) with: %r" % (mean, stdev, param))
 
 
+# Ensemble Method
+# 알고리즘의 성능을 향상시키는 또 하나의 방법으로 두 가지 종류가 존재한다. --> 배깅과 부스팅
+ensembles = []
+ensembles.append(('ScaledAB', Pipeline([('Scaler', StandardScaler()), ('AB', AdaBoostRegressor())])))
+ensembles.append(('ScaledGBM', Pipeline([('Scaler', StandardScaler()), ('GBM', GradientBoostingRegressor())])))
+ensembles.append(('ScaledRF', Pipeline([('Scaler', StandardScaler()), ('RF', RandomForestRegressor())])))
+ensembles.append(('ScaledET', Pipeline([('Scaler', StandardScaler()), ('ET', ExtraTreesRegressor())])))
+results = []
+names = []
+for name, model in ensembles:
+    kfold = KFold(n_splits=num_folds, random_state=seed, shuffle=True)
+    cv_results = cross_val_score(model, X_train, Y_train, cv=kfold, scoring=scoring)
+    results.append(cv_results)
+    names.append(name)
+    msg = "%s: %f (%f)" % (name, cv_results.mean(), cv_results.std())
+    print(msg)
+
+fig = pyplot.figure()
+fig.suptitle('Scaled Ensemble Algorithm Comparison')
+ax = fig.add_subplot(111)
+pyplot.boxplot(results)
+ax.set_xticklabels(names)
+pyplot.show()
+
+scaler = StandardScaler().fit(X_train)
+rescaledX = scaler.transform(X_train)
+param_grid = dict(n_estimators=numpy.array([50, 100, 150, 200, 250, 300, 350, 400]))
+model = GradientBoostingRegressor(random_state=seed)
+kfold = KFold(n_splits=num_folds, random_state=seed, shuffle=True)
+grid = GridSearchCV(estimator=model, param_grid=param_grid, scoring=scoring, cv=kfold)
+grid_result = grid.fit(rescaledX, Y_train)
+print("Best: %f using %s" % (grid_result.best_score_, grid_result.best_params_))
+means = grid_result.cv_results_['mean_test_score']
+stds = grid_result.cv_results_['std_test_score']
+params = grid_result.cv_results_['params']
+for mean, stdev, param in zip(means, stds, params):
+    print("%f (%f) with: %r " % (mean, stdev, param))
+
+
+scaler = StandardScaler().fit(X_train)
+rescaledX = scaler.transform(X_train)
+model = GradientBoostingRegressor(random_state=seed, n_estimators=400)
+model.fit(rescaledX, Y_train)
+
+rescaledValidationX = scaler.transform(X_validation)
+predictions = model.predict(rescaledValidationX)
+
+print(mean_squared_error(Y_validation, predictions))
